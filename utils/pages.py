8b08ea16a5ef70e0184988ac8d62b90171467f37
Glen Alexander <49417961+glenax@users.noreply.github.com>
2023-11-29 18:58:32 +1300

    Update pages.py

--- utils/pages.py
+++ utils/pages.py
@@ -4,13 +4,13 @@ import streamlit as st
 def set_page_config(title=""):
     st.set_page_config(
         page_title=title if len(title) > 0 else "PredictHQ Restaurant Demo App",
-        page_icon="🍔",
+        page_icon="🥯",
         layout="wide",
         initial_sidebar_state="expanded",
         menu_items={
             "Get help": "https://docs.predicthq.com",
             "About": """
-                **PredictHQ Restaurant Demo App**
+                **PredictHQ Amex Pick a Bagel Demo App**
 
                 PredictHQ Technical Documentation can be found at [https://docs.predicthq.com](https://docs.predicthq.com).
             """,
