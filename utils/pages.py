dd772f5b716a11f5a3b93b63cb4fce46e15e4154
Glen Alexander <49417961+glenax@users.noreply.github.com>
2023-11-29 18:59:06 +1300

    Update pages.py

--- utils/pages.py
+++ utils/pages.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def set_page_config(title=""):
     st.set_page_config(
-        page_title=title if len(title) > 0 else "PredictHQ Restaurant Demo App",
+        page_title=title if len(title) > 0 else "PredictHQ Amex Pick a Bagel Demo App",
         page_icon="🥯",
         layout="wide",
         initial_sidebar_state="expanded",
