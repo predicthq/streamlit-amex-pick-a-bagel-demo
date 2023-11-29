4080f052579460212e218c507dfcb3e923c24b9b
Glen Alexander <49417961+glenax@users.noreply.github.com>
2023-11-29 13:03:49 +1300

    Update map.py

--- map.py
+++ map.py
@@ -42,6 +42,12 @@ def map():
     ):
         return
 
+    st.markdown(
+        "<img src='app/static/amexlogo.png' width='160' />", unsafe_allow_html=True
+    )
+    st.markdown(
+        "<img src='app/static/pickabagel.png' width='160' />", unsafe_allow_html=True
+    )
     st.header(location["name"])
 
     # Display metrics
